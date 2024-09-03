96f520a4a9bdd96e167d0e1a332783e751d9e6ec
Brayan Acosta <landakkpp@gmail.com>
2024-09-03 10:22:24 -0500

    feat: basic structure backend

---
+++ linkup-ms/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linkup_ms.Models
+{
+    public class User
+    {
+        
+    }
+}
