94ae6e213290a0d4e291e2bce8906be2c25355e1
Brayan Acosta <landakkpp@gmail.com>
2024-09-09 09:55:47 -0500

    feat: add user model and user controller structure

---
+++ linkup-ms/Models/User.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linkup_ms.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public bool IsConfirmed { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Sector { get; set; }
+        [Required]
+        public UserRole Role { get; set; }
+    }
+}
