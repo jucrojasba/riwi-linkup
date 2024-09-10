222d3493e9c61ff934b37682d072aa0b5847a98b
18zuluaga <18zuluaga1989@gmail.com>
2024-09-10 09:39:49 -0500

    change property body styles

--- linkup-fr/src/app/login/page.tsx
+++ linkup-fr/src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import "./loginStyles.css";
 import { LogInForm } from "@/UI/components/molecules";
+import Image from "next/image";
 export default function LoginView(): JSX.Element{
     return(
         <main className="main">
             <section className="main-section">
-                <div>Image</div>
+                <div className="section-image">
+                    <Image className="image" src={"/images/test.png"} alt="coderLogin" width={100} height={100} />
+                </div>
                 <LogInForm />
             </section>
         </main>
