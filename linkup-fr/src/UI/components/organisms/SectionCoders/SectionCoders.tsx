02ad5ac6504da822e344756d91fb4b2f3dcce229
J <J>
2024-09-12 13:42:04 -0500

    change sectionCoders logic

--- linkup-fr/src/UI/components/organisms/SectionCoders/SectionCoders.tsx
+++ linkup-fr/src/UI/components/organisms/SectionCoders/SectionCoders.tsx
@@ -7,18 +7,20 @@ import { ICoder, ICoders } from "@/UI/interfaces/ICoderInterface";
 import { useEffect, useState } from "react";
 import { getCodersService } from "@/services/coderService";
 import { Card } from "../../molecules";
+import { CircularLoader } from "../../atoms/loaders/Loaders";
 
 export default function SectionCoders(): React.ReactElement {
+  const [loadingRequest, setLoadingRequest] = useState<boolean>(false);
   const initialCoder: ICoder = {
     id: 0,
-    url_image: "",
+    urlImage: "",
     name: "",
     birthday: "",
   };
   const initialCoders: ICoders = {
     coders: [initialCoder],
   };
-  
+
   const [coders, setCoders] = useState<ICoders>(initialCoders); // Full list of coders
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 8;
@@ -28,8 +30,10 @@ export default function SectionCoders(): React.ReactElement {
       const coders = await getCodersService();
       if (!coders) {
         console.log({ message: "Error getting users" });
+        setLoadingRequest(false);
         return;
       }
+      setLoadingRequest(true);
       setCoders({ coders });
     };
     getCoders();
@@ -37,7 +41,10 @@ export default function SectionCoders(): React.ReactElement {
 
   // Calculate paginated coders
   const startIndex = currentPage * itemsPerPage;
-  const paginatedCoders = coders.coders.slice(startIndex, startIndex + itemsPerPage);
+  const paginatedCoders = coders.coders.slice(
+    startIndex,
+    startIndex + itemsPerPage
+  );
 
   // Pagination handlers
   const handleNext = () => {
@@ -51,39 +58,46 @@ export default function SectionCoders(): React.ReactElement {
       setCurrentPage(currentPage - 1);
     }
   };
+  console.log(coders);
 
   return (
     <section className="main-section">
       <div className="section-content-cards">
-        {paginatedCoders.length > 0 ? (
-          paginatedCoders.map((coder: ICoder) => (
-            <Card
-              id_coder={coder.id}
-              key={coder.id}
-              url_image={
-                coder.url_image
-                  ? coder.url_image
-                  : "https://ehs.stanford.edu/wp-content/uploads/missing-image.png"
-              }
-              alt_image={`coder-${coder.name} image`}
-              name_user={coder.name}
-              age_user={`${calculateAge(coder.birthday)} years`}
-            />
-          ))
+        {loadingRequest ? (
+          <CircularLoader flag={true} />
         ) : (
-          <p>There are no coders...</p>
+          <CircularLoader flag={false} />
         )}
+        {coders.coders.map((coder) => (
+          <Card
+            id_coder={coder.id}
+            key={coder.id}
+            url_image={coder.urlImage}
+            alt_image={`coder-${coder.name} image`}
+            name_user={coder.name}
+            age_user={`${calculateAge(coder.birthday)} years`}
+          />
+        ))}
       </div>
       <div className="section-buttons">
         <KeyboardArrowLeftIcon
           className="button-left"
           onClick={handlePrevious}
-          style={{ cursor: 'pointer', visibility: currentPage === 0 ? 'hidden' : 'visible' }}
+          style={{
+            cursor: "pointer",
+            visibility: currentPage === 0 ? "hidden" : "visible",
+          }}
         />
         <KeyboardArrowRightIcon
           className="button-right"
           onClick={handleNext}
-          style={{ cursor: 'pointer', visibility: (currentPage + 1) * itemsPerPage >= coders.coders.length ? 'hidden' : 'visible' }}
+          style={{
+            cursor: "pointer",
+            visibility:
+              (currentPage + 1) * itemsPerPage >= coders.coders.length
+                ? "hidden"
+                : "visible",
+          }}
         />
       </div>
     </section>
--- linkup-fr/src/utilities/fetchApi.ts
+++ linkup-fr/src/utilities/fetchApi.ts
@@ -6,8 +6,6 @@ interface IOptionsProps{
 
 export default async function fetchApi(url:string,options?:IOptionsProps):Promise<any>{
     try{
-        const url:string = "http://192.168.88.72:5298/" + path;
-        console.log(url);
         const response = await fetch(url,options);
         if(!response.ok)throw new Error("Error with the response");
         return await response.json();
