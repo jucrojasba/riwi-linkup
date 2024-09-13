4883cf5054088efe38f54bbf0424ff4c809c2306
Juan Pablo Arias <juanpabloarias803@gmail.com>
2024-09-13 12:05:26 -0500

    add login dark mode

--- linkup-fr/src/UI/components/molecules/LogInForm/LogInForm.tsx
+++ linkup-fr/src/UI/components/molecules/LogInForm/LogInForm.tsx
@@ -6,6 +6,7 @@ import CustomLink from "../../atoms/CustomLink/CustomLink";
 import PasswordInput from "../../atoms/PasswordInput/PasswordInput";
 import { useState } from "react";
 import { ICompanyLogin } from "@/UI/interfaces/Forms";
+import { useDarkMode } from "@/global-states/dark-mode";
 
 const CompanyInitialState={
     email:'',
@@ -16,6 +17,7 @@ const LogInForm:React.FC=()=>{
 
     const[passwordInputError,setPasswordInputError] =useState(false); // Este estado cambia si se hacen malas peticiones al servidor
     const[companyRegister,setCompanyRegister] =useState<ICompanyLogin>(CompanyInitialState);
+    const DarkMode = useDarkMode((state) => state.DarkMode);
 
     function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
         setCompanyRegister((prevState) => ({
@@ -32,7 +34,7 @@ const LogInForm:React.FC=()=>{
             <MainButton text="Log In" onClick={()=>{}}></MainButton>
             <CustomLink text="Forgot Password?" href="/recover-password"></CustomLink>
             <Box component={'span'}>
-                <Typography variant="body1" sx={{color:'var(--secondary-color)',fontFamily:'var(--main-font)'}}>Do not have an account? <CustomLink text="Sign up" href="/register"></CustomLink></Typography>
+                {DarkMode?<Typography variant="body1" sx={{color:'var(--white-color)',fontFamily:'var(--main-font)'}}>Do not have an account? <CustomLink text="Sign up" href="/register"></CustomLink></Typography>:<Typography variant="body1" sx={{color:'var(--secondary-color)',fontFamily:'var(--main-font)'}}>Do not have an account? <CustomLink text="Sign up" href="/register"></CustomLink></Typography>}
             </Box>
         </Box>
     );
--- linkup-fr/src/app/login/page.tsx
+++ linkup-fr/src/app/login/page.tsx
@@ -1,15 +1,18 @@
 'use client'
 import "./loginStyles.css";
 import { LogInForm } from "@/UI/components/molecules";
-import NavbarHome from "@/UI/components/molecules/NavbarHome/NavbarHome";
+import { AuthLayout } from "@/UI/components/organisms";
+import { useDarkMode } from "@/global-states/dark-mode";
 import Image from "next/image";
+
 export default function LoginView(): JSX.Element{
+    const DarkMode = useDarkMode((state) => state.DarkMode);
     return(
         <main className="main">
-            <NavbarHome/>
-            <section className="main-section">
+            <AuthLayout isDarkMode={DarkMode} />
+            <section className={DarkMode?"dark-mode": "main-login-section"}>
                 <div className="section-image">
-                    <Image className="image" src={"/images/test.png"} alt="coderLogin" width={100} height={100} />
+                    <Image className="image" src={"/images/test.png"} alt="coderLogin" width={600} height={600} />
                 </div>
                 <LogInForm />
             </section>
