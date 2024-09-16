7361bf9241edd18052e72dcb7dc59e49c64a389b
Juan Pablo Arias <juanpabloarias803@gmail.com>
2024-09-16 08:40:34 -0500

    Fixing deploy errors

--- linkup-fr/src/app/admin/page.tsx
+++ linkup-fr/src/app/admin/page.tsx
@@ -3,11 +3,13 @@ import { useLanguage } from "@/global-states/language-mode";
 import { DashboardLayout } from "@/UI/components/organisms";
 import { SectionCoders } from "@/UI/components/organisms";
 import Route from "@/routes/route";
+import { useDarkMode } from "@/global-states/dark-mode";
 export default function AdminView(): React.ReactNode {
   const languages = useLanguage();
+  const DarkMode = useDarkMode((state) => state.DarkMode);
   return (
     <Route>
-      <DashboardLayout section={<SectionCoders />} titleView="Coders" subtitle="General Information" path="/admin" language={languages}/>
+      <DashboardLayout section={<SectionCoders />} titleView="Coders" subtitle="General Information" path="/admin" language={languages} isDarkMode={DarkMode}/>
     </Route>
   );
 }
