71506594d8887ac76926a1c0d03bfc32a469c8b1
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-09-17 08:17:55 -0500

    Add preview config view

--- linkup-fr/src/app/config/page.tsx
+++ linkup-fr/src/app/config/page.tsx
@@ -1,3 +1,25 @@
-'use client'
+'use client';
+import { useLanguage } from '@/global-states/language-mode';
+import { useDarkMode } from "@/global-states/dark-mode";
+import { DashboardLayout } from '@/UI/components/organisms';
+import SectionDashboard from '@/UI/components/organisms/SectionDashboard/SectionDashboard';
+import { useAuthUser } from '@/global-states/authUser';
+import Route from '@/routes/route';
 
-import './configStyles.css'
+
+export default function DashboardView(){
+    // Logic
+    const DarkMode = useDarkMode((state) => state.DarkMode);
+    const language=useLanguage((state) => state.language);
+    const titleView = language? "Tablero":"Dashboard";
+    const subtitle= language? "Estadisticas Globales":"General Stats";
+    const AuthUser = useAuthUser((state)=> state.authUser);
+
+    return (
+        <Route>
+            <main>
+                <DashboardLayout section={<SectionDashboard isDarkMode={DarkMode} language={language}/>} language={language} titleView={titleView} subtitle={subtitle} path="/dashboard" isDarkMode={DarkMode}/>
+            </main>
+        </Route>
+    );
+};
