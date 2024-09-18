b5231e603c96e014eb508da13198646578e51333
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-09-18 06:55:00 -0500

    add next auth secret

--- linkup-fr/src/app/api/auth/[...nextauth]/route.ts
+++ linkup-fr/src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID as string,
