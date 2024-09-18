130ba1a000a84c93443b06044be1d0cf3b2e9a0f
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-09-18 06:53:47 -0500

    Change route of file section coder only

--- linkup-fr/src/UI/components/organisms/index.ts
+++ linkup-fr/src/UI/components/organisms/index.ts
@@ -2,7 +2,7 @@ import AuthLayout from "./AuthLayout/AuthLayout";
 import DashboardLayout from "./DashboardLayout/DashboardLayout";
 import SectionCoders from "./SectionCoders/SectionCoders";
 import DashboardCardsContainer from "./DashboardCardsContainer/DashboardCardsContainer";
-import SectionCoderOnly from "./sectionCoderOnly/SectionCoderOnly";
+import SectionCoderOnly from "./sectionCoderOnly/sectionCoderOnly";
 
 export {
     AuthLayout,
