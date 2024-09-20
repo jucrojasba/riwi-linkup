3f4dd601dbf50798891c99203a24bd475488304e
jose cardona <josesiprozmaster@gmail.com>
2024-09-20 10:24:07 -0500

    add logic register view

---
+++ linkup-fr/src/services/sectorService.ts
@@ -0,0 +1,9 @@
+import { ISector } from "@/UI/interfaces/ISectorInterface";
+import fetchApi from "@/utilities/fetchApi";
+
+export async function getSectorService(): Promise<
+  { data: ISector[] } | null | undefined | { message: string }
+> {
+  const data = await fetchApi("/api/sectors");
+  return data;
+}
